74c518a87a2132584edd9da560af95ba064f99a2
shakianand <sakila.m@gmail.com>
2020-06-01 14:57:36 +0530

    code added

---
+++ src/org/test/SampleProject.java
@@ -0,0 +1,26 @@
+package org.test;
+
+public class SampleProject {
+	
+	private void CourseDetails() {
+		// TODO Auto-generated method stub
+		System.out.println("Java Started");
+
+	}
+	
+	private void SeleniumDetails() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Selenium not started");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		SampleProject s = new SampleProject();
+		s.CourseDetails();
+		s.SeleniumDetails();
+		
+	}
+
+}
